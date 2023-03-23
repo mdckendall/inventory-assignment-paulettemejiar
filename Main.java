3b82197032f699ed2467b43db9f4cf670a26c653
Paulette Mejia <124909264+paulettemejiar@users.noreply.github.com>
2023-03-23 15:14:37 +0000

    moved inventory

--- Main.java
+++ Main.java
@@ -2,6 +2,45 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
+class Inventory{
+   private String name;
+   private String serial;
+   private int price;
+
+  public Inventory(String name,String serial,int price){
+    this.name =name;
+    this.serial =serial;
+    this.price =price;
+
+  }
+   public void setName(String name){
+     this.name =name;
+     
+   }
+   public String getName(){
+     return name;
+   }
+   public void setSerial(String serial){
+     this.serial =serial;
+   }
+   public String getSerial(){
+     return serial;
+   }
+   public void setPrice(int price){
+     this.price =price;
+   }
+   public int getPrice(){
+     return price;
+   }
+   @Override
+   public String toString(){
+     return name+ "," + serial+ "," + price;
+   }
+   
+
+   
+   }
+
 class Main {
  
 
